f02ed9519039edff3a9518b8dd3ca567bbc1f5cf
Ivan Shamatov <status.enable@gmail.com>
2017-08-03 18:49:50 +0300

    grape integartion fix

--- lib/xyeger/integrations/grape.rb
+++ lib/xyeger/integrations/grape.rb
@@ -4,8 +4,8 @@ module Xyeger
   module Grape
     module Logger
       def self.included(_base)
-        Grape::API.logger.extend(Xyeger::Logger)
-        Grape::API.logger.formatter = Xyeger.config.formatter
+        ::Grape::API.logger.extend(Xyeger::Logger)
+        ::Grape::API.logger.formatter = Xyeger.config.formatter
       end
     end
   end
