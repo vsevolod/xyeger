9c8fb563d6bc78d9de18805adc16a8e7b6ee5eaa
Ivan Shamatov <status.enable@gmail.com>
2017-08-03 12:21:31 +0300

    revert config initializer

--- lib/xyeger/config.rb
+++ lib/xyeger/config.rb
@@ -1,37 +1,15 @@
 module Xyeger
   class Config
-    attr_writer(
-      :output, :formatter,
-      :app, :env, :hostname,
-      :context, :context_resolver
-    )
+    attr_accessor :output, :formatter, :hostname, :app, :env, :context, :context_resolver
 
-    def output
-      @output ||= STDOUT
-    end
-
-    def formatter
-      @formatter ||= Xyeger::Formatters::Json.new
-    end
-
-    def hostname
-      @hostname ||= ENV['XYEGER_HOSTNAME']
-    end
-
-    def app
-      @app ||= ENV['XYEGER_APPNAME']
-    end
-
-    def env
-      @env ||= ENV['XYEGER_ENV']
-    end
-
-    def context
-      @context ||= Xyeger::Context.new
-    end
-
-    def context_resolver
-      @context_resolver ||= Xyeger::ContextResolver.new
+    def initialize
+      @output = STDOUT
+      @formatter = Xyeger::Formatters::Json.new
+      @hostname = ENV['XYEGER_HOSTNAME']
+      @app = ENV['XYEGER_APPNAME']
+      @env = ENV['XYEGER_ENV']
+      @context = Xyeger::Context.new
+      @context_resolver = Xyeger::ContextResolver.new
     end
   end
 end
